46b7e76b4f0c4eed49066131fca3fc65385e0d9c
Omar Mohamed Ahmed <45721757+OmarMohammed88@users.noreply.github.com>
2020-05-11 13:07:06 +0200

    Add files via upload

---
+++ Interpreter.py
@@ -0,0 +1,152 @@
+from RTResult import *
+from Number import *
+
+#identifiers  type
+
+
+TT_INT				= 'INT'
+TT_FLOAT    	= 'FLOAT'
+TT_IDENTIFIER	= 'IDENTIFIER'
+TT_KEYWORD		= 'KEYWORD'
+TT_PLUS     	= 'PLUS'
+TT_MINUS    	= 'MINUS'
+TT_MUL      	= 'MUL'
+TT_DIV      	= 'DIV'
+TT_POW				= 'POW'
+TT_EQ					= 'EQ'
+TT_LPAREN   	= 'LPAREN'
+TT_RPAREN   	= 'RPAREN'
+TT_EE					= 'EE'
+TT_NE					= 'NE'
+TT_LT					= 'LT'
+TT_GT					= 'GT'
+TT_LTE				= 'LTE'
+TT_GTE				= 'GTE'
+TT_EOF				= 'EOF'
+
+KEYWORDS = [
+	'VAR',
+	'AND',
+	'OR',
+	'NOT',
+	'IF',
+	'THEN',
+	'ELIF',
+	'ELSE'
+]
+
+
+class Interpreter:
+	def visit(self, node, context):
+		method_name = f'visit_{type(node).__name__}'
+		method = getattr(self, method_name, self.no_visit_method)
+		return method(node, context)
+
+	def no_visit_method(self, node, context):
+		raise Exception(f'No visit_{type(node).__name__} method defined')
+
+	###################################
+
+	def visit_NumberNode(self, node, context):
+		return RTResult().success(
+			Number(node.tok.value).set_context(context).set_pos(node.pos_start, node.pos_end)
+		)
+
+	def visit_VarAccessNode(self, node, context):
+		res = RTResult()
+		var_name = node.var_name_tok.value
+		value = context.symbol_table.get(var_name)
+
+		if not value:
+			return res.failure(RTError(
+				node.pos_start, node.pos_end,
+				f"'{var_name}' is not defined",
+				context
+			))
+
+		value = value.copy().set_pos(node.pos_start, node.pos_end)
+		return res.success(value)
+
+	def visit_VarAssignNode(self, node, context):
+		res = RTResult()
+		var_name = node.var_name_tok.value
+		value = res.register(self.visit(node.value_node, context))
+		if res.error: return res
+
+		context.symbol_table.set(var_name, value)
+		return res.success(value)
+
+	def visit_BinOpNode(self, node, context):
+		res = RTResult()
+		left = res.register(self.visit(node.left_node, context))
+		if res.error: return res
+		right = res.register(self.visit(node.right_node, context))
+		if res.error: return res
+
+		if node.op_tok.type == TT_PLUS:
+			result, error = left.added_to(right)
+		elif node.op_tok.type == TT_MINUS:
+			result, error = left.subbed_by(right)
+		elif node.op_tok.type == TT_MUL:
+			result, error = left.multed_by(right)
+		elif node.op_tok.type == TT_DIV:
+			result, error = left.dived_by(right)
+		elif node.op_tok.type == TT_POW:
+			result, error = left.powed_by(right)
+		elif node.op_tok.type == TT_EE:
+			result, error = left.get_comparison_eq(right)
+		elif node.op_tok.type == TT_NE:
+			result, error = left.get_comparison_ne(right)
+		elif node.op_tok.type == TT_LT:
+			result, error = left.get_comparison_lt(right)
+		elif node.op_tok.type == TT_GT:
+			result, error = left.get_comparison_gt(right)
+		elif node.op_tok.type == TT_LTE:
+			result, error = left.get_comparison_lte(right)
+		elif node.op_tok.type == TT_GTE:
+			result, error = left.get_comparison_gte(right)
+		elif node.op_tok.matches(TT_KEYWORD, 'AND'):
+			result, error = left.anded_by(right)
+		elif node.op_tok.matches(TT_KEYWORD, 'OR'):
+			result, error = left.ored_by(right)
+
+		if error:
+			return res.failure(error)
+		else:
+			return res.success(result.set_pos(node.pos_start, node.pos_end))
+
+	def visit_UnaryOpNode(self, node, context):
+		res = RTResult()
+		number = res.register(self.visit(node.node, context))
+		if res.error: return res
+
+		error = None
+
+		if node.op_tok.type == TT_MINUS:
+			number, error = number.multed_by(Number(-1))
+		elif node.op_tok.matches(TT_KEYWORD, 'NOT'):
+			number, error = number.notted()
+
+		if error:
+			return res.failure(error)
+		else:
+			return res.success(number.set_pos(node.pos_start, node.pos_end))
+
+	def visit_IfNode(self, node, context):
+		res = RTResult()
+
+		for condition, expr in node.cases:
+			condition_value = res.register(self.visit(condition, context))
+			if res.error: return res
+
+			if condition_value.is_true():
+				expr_value = res.register(self.visit(expr, context))
+				if res.error: return res
+				return res.success(expr_value)
+
+		if node.else_case:
+			else_value = res.register(self.visit(node.else_case, context))
+			if res.error: return res
+			return res.success(else_value)
+
+		return res.success(None)
